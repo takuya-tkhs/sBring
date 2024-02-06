80eec376c98b03e06c00276a456df61e9e365216
takuya-tkhs <takuya.14966@gmail.com>
2024-02-06 16:41:13 +0100

    Transmission rate by Gaussian distribution.

--- code_for_SDS/weighted_adj_list_from_locations.R
+++ code_for_SDS/weighted_adj_list_from_locations.R
@@ -4,7 +4,19 @@ library(tidyverse)
 
 location_file_name <- "ortsnetz.txt"
 gamma <- 10 ^ 4  #i.e., 10 km
+sigma <- 10 ^ 4
 
+transmission_rate_mat_cauchy <- function(dist_mat, gamma){
+  return(1 / (dist_mat ^ 2 + gamma ^ 2))
+}
+
+transmission_rate_mat_gaussian <- function(dist_mat, sigma){
+  return(exp(- dist_mat ^ 2 / (2 * sigma ^ 2)))
+}
+
+
+
+#main routine starts here.
 sds_network <- fread(location_file_name, encoding = "UTF-8")
 num_location <- nrow(sds_network)
 
@@ -18,11 +30,12 @@ for(i in 1:num_location){
 }
 remove(sds_dist_mat_tmp)
 
-transmission_rate_mat <- 1 / (sds_dist_mat ^ 2 + gamma ^ 2)
+transmission_rate_mat <- transmission_rate_mat_gaussian(sds_dist_mat, sigma)
+#transmission_rate_mat <- transmission_rate_mat_cauchy(sds_dist_mat, gamma)
 for(i in 1:num_location){
   tmp_sum <- sum(transmission_rate_mat[i,])
   transmission_rate_mat[i,] <- transmission_rate_mat[i,] / tmp_sum
 }
 
-write.csv(transmission_rate_mat, "weighted_adj_mat.csv",
+write.csv(transmission_rate_mat, "weighted_adj_mat_gaussian_10km.csv",
           row.names = F)
